952c9042b8971860f5cab682452815b79f08a6fe
karthikjohnn <karthickyamkk2001@gmail.com>
2024-12-07 12:58:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
